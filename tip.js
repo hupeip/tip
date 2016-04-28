aad762772084f731c313e234c8c182a06daeabd8
hupeip <hupei_323@163.com>
2016-04-28 15:17:27 +0800

    增加注释

--- tip.js
+++ tip.js
@@ -1,6 +1,7 @@
 
 $(function($){	
 	$.fn.tip=function(options){
+		//定义默认参数
 		var defaults={
 			content:'',
 			placement:'top',
@@ -9,7 +10,10 @@ $(function($){
 			color:'#000',
 			template:'<div class="popover" role="tooltip"><div class="arrow"><em></em><i></i></div><div class="popover-content"></div></div>'
 		};
+		//将传进来的参数与默认参数合并，相同的就取options,没有传的就去defaults
 		var options=$.extend({},defaults,options);
+
+		//定义显示提示的方法，将选择的对象传进来
 		function tipShow(el){
 			// console.log(options['content']);
 			$(el).after(options.template);
@@ -57,6 +61,7 @@ $(function($){
 			
 
 		}
+		//这里是调用上面的方法，在javascript中能够链式调用
 		return this.each(function() {
 			if (options.trigger=='click') {
 				$(this).click(function() {
